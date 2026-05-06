37f601f2102844d9fee82d1eb43e6662812ac88d
Ahmed Bedir <243879194+Ahmed-Bedir0@users.noreply.github.com>
2026-05-06 23:09:53 +0300

    Add files via upload

---
+++ Backend/Backend/src/test/java/com/premierzone/Backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.premierzone.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
